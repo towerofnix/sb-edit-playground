75836e496c2bcebc1f296f90e389b8a2a3118eef
Florrie <towerofnix@gmail.com>
2020-02-11 19:48:12 -0400

    Extensibleify

--- README.md
+++ README.md
@@ -1,5 +1,33 @@
 # sb-edit-playground
 
-## setup
+## Setup
 
-just clone, npm install, and link (your-dir-to-scratch-js)/scratch-js (the directory that contains the mjs files) to sb-edit-playground/scratch-js if you plan on testing scratch-js with this! you'll probably want to mess with index.js to adjust it to your liking, i haven't coded it to be full-featured without editing that file yet
+```
+# this setup assumes scratch-js & sb-edit exist in pwd
+# it also assumes you have run npm link in sb-edit
+git clone https://github.com/towerofnix/sb-edit-playground
+cd sb-edit-playground
+ln -s ../scratch-js/scratch-js scratch-js
+ln -s ../sb-edit/src sb-edit
+npm link sb-edit
+
+# to run sb-edit:
+# cd sb-edit-playground
+node index.js (my file.sb3) (output-format)
+# result will be in sb-edit-playground/out/(output-format)
+# run index.js without an output format to get a list
+
+# while developing sb-edit:
+# cd sb-edit-playground/sb-edit
+npm run watch
+
+# while testing scratch-js output:
+# cd sb-edit-playground
+npx http-server -p 3001 # or your favorite port
+
+# git works in the linked directories!
+# it will target scratch-js or sb-edit accordingly
+# also be sure to watch out when undoing changes, etc
+# always do a git status before you accidentally run
+# git checkout -- . in the wrong repository 0_0
+```
--- index.js
+++ index.js
@@ -4,53 +4,81 @@ const util = require('util');
 const path = require('path');
 
 const mkdirpp = util.promisify(require('mkdirp'));
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+function progressPromiseAll(msg, array) {
+    let done = 0, total = array.length;
+    process.stdout.write(`\r${msg} [0/${total}]`);
+    return Promise.all(array.map(promise => promise.then(val => {
+        done++;
+        if (done === total) {
+            process.stdout.write(`\r\x1b[2m${msg} [${done}/${total}] \x1b[0;32mDone!\x1b[0m \n`)
+        } else {
+            process.stdout.write(`\r${msg} [${done}/${total}]`);
+        }
+        return val;
+    })));
+};
+
+const outputHandlers = {
+    async "scratch-js"(project, util) {
+        await progressPromiseAll('Writing files', Object.entries(project.toScratchJS({
+            scratchJSURL: "/scratch-js/index.mjs",
+            scratchJSCSSURL: "/scratch-js/index.css",
+            getAssetURL: util.getAssetPath
+        })).map(([ filename, contents ]) => util.writeQuiet(filename, contents)));
+        await util.writeAssets();
+    },
+    async "scratchblocks"(project, util) {
+        await progressPromiseAll('Writing files', Object.entries(project.toScratchblocks())
+            .map(([ target, contents ]) => util.writeQuiet(target + '.txt', contents))
+        );
+    }
+};
 
 async function main() {
     if (!process.argv[2]) {
-        console.error('Please specify an sb3 to open.');
+        console.error("Please specify an sb3 to open.");
         process.exit(1);
     }
 
-    const file = fs.readFileSync(process.argv[2]);
+    if (!process.argv[3] || !Object.keys(outputHandlers).includes(process.argv[3])) {
+        console.error("Please specify an output format:");
+        console.error(Object.keys(outputHandlers).map(line => "- " + line).join("\n"));
+        process.exit(1);
+    }
+
+    const file = await readFile(process.argv[2]);
     const project = await Project.fromSb3(file);
 
-    /*
-    for (const target of [project.stage, ...project.sprites]) {
-        console.log(target.name);
-        for (const script of target.scripts) {
-            console.log(script);
-        }
-    }
-    */
-
-    for (const [ filename, contents ] of Object.entries(project.toScratchJS({
-        scratchJSURL: "/scratch-js/index.mjs",
-        scratchJSCSSURL: "/scratch-js/index.css"
-    }))) {
-        const out = 'out/' + filename;
-        await mkdirpp(path.dirname(out));
-        fs.writeFileSync(out, contents);
-        console.log(out);
-    }
-
-    for (const target of [project.stage, ...project.sprites]) {
-        for (const costume of target.costumes) {
-            const out = 'out/' + target.name + '/costumes/' + costume.name + '.' + costume.ext;
+    const util = {
+        getAssetPath({md5, ext}) {
+            return `${md5}.${ext}`;
+        },
+        getOutputDirectory() {
+            return path.join("out", process.argv[3]);
+        },
+        async write(filename, data) {
+            const out = path.join(this.getOutputDirectory(), filename);
+            process.stdout.write(out + "...");
+            await this.writeQuiet(filename, data);
+            process.stdout.write("done!\n");
+        },
+        async writeQuiet(filename, data) {
+            const out = path.join(this.getOutputDirectory(), filename);
             await mkdirpp(path.dirname(out));
-            fs.writeFileSync(out, Buffer.from(costume.asset));
-            console.log(out);
+            await writeFile(out, data);
+        },
+        async writeAssets() {
+            const allAssets = [project.stage, ...project.sprites].reduce((acc, target) => acc.concat(target.costumes, target.sounds), []);
+            await progressPromiseAll('Writing assets', allAssets.map(asset => this.writeQuiet(this.getAssetPath(asset), Buffer.from(asset.asset))));
         }
-    }
+    };
 
-    /*
-    if (!process.argv[3]) {
-        console.error('Please specify a target to convert.');
-        process.exit(1);
-    }
+    await outputHandlers[process.argv[3]](project, util);
 
-    // console.log(project.toScratchJS()[`${process.argv[3]}/${process.argv[3]}.mjs`]);
-    console.log(project.toScratchblocks()[process.argv[3]]);
-    */
+    console.log(`Wrote to directory ${util.getOutputDirectory()}`);
 }
 
 main().catch(err => console.error(err));
