29099c8ccb011ec1087c7e3095714bbb2034c35c
Florrie <towerofnix@gmail.com>
2020-02-13 14:08:20 -0400

    support "data" & sb3 format, new "zip" util

--- index.js
+++ index.js
@@ -70,12 +70,22 @@ const outputHandlers = {
             return `${index}.${asset.ext}`;
         });
 
-        const out = util.getOutputDirectory();
-        await promisifyProcess(spawn("zip", [
-            path.join(out, "out-sb2.sb2"),
-            ...(await readdir(out)).map(file => path.join(out, file))
-        ]));
+        await util.zip("sb2");
     },
+
+    async "sb3"(project, util) {
+        if (!project.toSb3) {
+            console.error("toSb3 isn't implemented!");
+            process.exit(1);
+        }
+
+        const { json } = project.toSb3({});
+        const prettyJSON = stringify(JSON.parse(json));
+        await util.write("project.json", prettyJSON);
+        await util.writeAssets();
+        await util.zip("sb3");
+    },
+
     async "scratch-js"(project, util) {
         await progressPromiseAll('Writing files', Object.entries(project.toScratchJS({
             scratchJSURL: "/scratch-js/index.mjs",
@@ -84,6 +94,7 @@ const outputHandlers = {
         })).map(([ filename, contents ]) => util.writeQuiet(filename, contents)));
         await util.writeAssets();
     },
+
     async "scratchblocks"(project, util) {
         await progressPromiseAll('Writing files', Object.entries(project.toScratchblocks())
             .map(([ target, contents ]) => util.writeQuiet(target + '.txt', contents))
@@ -106,6 +117,8 @@ async function main() {
     const file = await readFile(process.argv[2]);
     const project = await Project.fromSb3(file);
 
+    const basename = path.basename(process.argv[2], path.extname(process.argv[2]));
+
     const util = {
         getAssetPath({md5, ext}) {
             return `${md5}.${ext}`;
@@ -126,7 +139,14 @@ async function main() {
         },
         async writeAssets(getAssetPath = this.getAssetPath) {
             const allAssets = [project.stage, ...project.sprites].reduce((acc, target) => acc.concat(target.costumes, target.sounds), []);
-            await progressPromiseAll('Writing assets', allAssets.map(asset => this.writeQuiet(getAssetPath(asset), Buffer.from(asset.asset))));
+            await progressPromiseAll('Writing assets', allAssets.map(asset => this.writeQuiet(getAssetPath(asset), Buffer.from(asset.data || asset.asset))));
+        },
+        async zip(ext = "zip") {
+            const out = this.getOutputDirectory();
+            await promisifyProcess(spawn("zip", [
+                path.join(out, `${basename}.${ext}`),
+                ...(await readdir(out)).map(file => path.join(out, file))
+            ]));
         }
     };
 
